7b47e5afd3ecce748071f1b70d33c6ce6e189866
bruno250km <47260237+bruno250km@users.noreply.github.com>
2019-02-01 22:02:00 -0200

    Update deploy_dockerhub.sh

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -2,5 +2,5 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 TAG="$TRAVIS_BUILD_NUMBER"
 
-docker build -f Dockerfile -t devfiap/BRUNOPRESIDA:$TAG .
-docker push devfiap/BRUNOPRESIDA
+docker build -t devfiap/brunopresida:$TAG .
+docker push devfiap/brunopresida
