8641e364e95ba255706fdf7d2667f3d189bab8bf
bruno250km <47260237+bruno250km@users.noreply.github.com>
2019-02-01 22:14:50 -0200

    Update generator.py

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+buzz = ('vamos para grilla', 'chamadiiiiinhaaaaaaaaa',
+    'nao vim fazer amizades', 'hahahahahaaa', 'vamosssssssss   emmm booooraaaaa')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
